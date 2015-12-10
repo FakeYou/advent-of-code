85c3e3d577e7d4718d5571d580fb40d9975825a9
unknown <ag.nanninga@gmail.com>
2015-12-10 11:11:42 +0100

    Added comments to day 10

--- day-10/solve.py
+++ day-10/solve.py
@@ -5,26 +5,41 @@ import time
 
 input = 1321131112
 
+# method to look at a number and say the digits in it
+# https://en.wikipedia.org/wiki/Look-and-say_sequence
+#
+# for example:
+#
+# input: 1211
+# output: 111221 (one 1, one 2, two 1s)
 def look_and_say(number):
     newNumber = ''
+    # group all repeating digits    
     pattern = re.compile(r'(\d)\1*')
     groups = [match.group() for match in pattern.finditer(str(number))]
 
+    # for every group first add the amount of the group and then the
+    # digit of the group to the newNumber
     for group in groups:
         newNumber += str(len(group)) + str(group[0])
 
+    # return the new number
     return newNumber
 
 def part1(input):
+    # execute look_and_say 40 times on the input
     for i in range(0, 40):
         input = look_and_say(input)
 
+    # return the length of the final number
     return len(input)
 
 def part2(input):
+    # execute look_and_say 50 times on the input
     for i in range(0, 50):
         input = look_and_say(input)
 
+    # return the length of the final number
     return len(input)
 
 
