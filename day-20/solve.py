2c93e4b51f7ed72b72432d852a917bb955e01d67
Andre Nanninga <ag.nanninga@gmail.com>
2015-12-20 15:59:38 +0100

    solution to day 20

--- day-20/solve.py
+++ day-20/solve.py
@@ -2,7 +2,7 @@
 
 import time
 from pprint import pprint
-from collections import Counter
+from collections import Counter, defaultdict
 
 threshold = 36000000
 
@@ -12,10 +12,23 @@ def part1(threshold):
         for house in range(elf, round(threshold/10), elf):
             houses[house] += elf * 10
 
-        print(elf)
+            if houses[elf] >= threshold:
+                return house
 
-    for house, presents in houses.items():
-        if presents == threshold:
-            return house
+def part2(threshold):
+    houses = Counter()
 
-print(["--", part1(threshold)])
+    for elf in range(1, round(threshold/10)):
+        for house in range(elf, elf * 50 + 1, elf):
+            houses[house] += elf * 11
+
+            if houses[elf] >= threshold:
+                return house
+
+start = time.time()
+print("Solution to part 1: %s" % part1(threshold))
+print("Duration: %s seconds" % str(time.time() - start))
+
+start = time.time()
+print("Solution to part 2: %s" % part2(threshold))
+print("Duration: %s seconds" % str(time.time() - start))
